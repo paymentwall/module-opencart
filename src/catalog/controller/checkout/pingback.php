08ef9c744c1bf8525e361406f4e2cc1a17b6b8ab
David Hoang <david.hoang@paymentwall.com>
2017-02-14 05:29:49 +0000

    Update Subscription for PwLocal method

--- src/catalog/controller/checkout/pingback.php
+++ src/catalog/controller/checkout/pingback.php
@@ -5,7 +5,14 @@ class ControllerCheckoutPingback extends Controller
     protected $paymentModel;
 
     const DEFAULT_PINGBACK_RESPONSE_SUCCESS = "OK";
-
+    const TRANSACTION_CREATED = 0;
+    const TRANSACTION_COMPLETED = 1;
+    const TRANSACTION_CANCELED = 5;
+    const TRANSACTION_SKIPPED = 3;
+    const RECURRING_PEDING = 6;
+    const RECURRING_ACTIVE = 2;
+    const RECURRING_CANCELED = 4;
+    
     public function index()
     {
         $this->load->model('setting/setting');
@@ -31,35 +38,75 @@ class ControllerCheckoutPingback extends Controller
         }
 
         if ($pingback->validate()) {
+            $referenceId = $pingback->getReferenceId();
+            $transactionStatus = self::TRANSACTION_CREATED;
+            $recurringStatus = self::RECURRING_PEDING;
 
             if ($pingback->isDeliverable()) {
-                $this->getPaymentModel()->callDeliveryApi($order, $pingback->getReferenceId());
+                $transactionStatus = self::TRANSACTION_COMPLETED;
+                $recurringStatus = self::RECURRING_ACTIVE;
 
-                if ($order['order_status_id'] != $this->config->get($order['payment_code'] . '_complete_status')) {
-                    $this->getCheckoutOrderModel()->update(
-                        $orderId,
-                        $this->config->get($order['payment_code'] . '_complete_status'),
-                        'Order approved!, Transaction Id: #' . $pingback->getReferenceId(),
-                        true
-                    );
-                }
+                $this->getPaymentModel()->callDeliveryApi($order, $referenceId);
+
+                $this->getCheckoutOrderModel()->update(
+                    $orderId,
+                    $this->config->get($order['payment_code'] . '_complete_status'),
+                    'Order approved!, Transaction Id: #' . $referenceId,
+                    true
+                );
 
             } elseif ($pingback->isCancelable()) {
-                if ($order['order_status_id'] != $this->config->get($order['payment_code'] . '_cancel_status')) {
-                    $this->getCheckoutOrderModel()
-                        ->update($orderId, $this->config->get($order['payment_code'] .'_cancel_status'), 'Order canceled!', true);
-                }
+                $transactionStatus = self::TRANSACTION_CANCELED;
+                $recurringStatus = self::RECURRING_CANCELED;
+
+                $this->getCheckoutOrderModel()
+                    ->update($orderId, $this->config->get($order['payment_code'] .'_cancel_status'), 'Order canceled!', true);
             } elseif ($pingback->isUnderReview()) {
-                if ($order['order_status_id'] != $this->config->get($order['payment_code'] . '_under_review_status')) {
-                    $this->getCheckoutOrderModel()
-                        ->update($orderId, $this->config->get($order['payment_code'] .'_under_review_status'), 'The order is under review!', false);
-                }
+                $this->getCheckoutOrderModel()
+                    ->update($orderId, $this->config->get($order['payment_code'] .'_under_review_status'), 'The order is under review!', true);
             }
+
+            $this->updateRecurringOrder($order, $referenceId, $transactionStatus, $recurringStatus);
 
             echo self::DEFAULT_PINGBACK_RESPONSE_SUCCESS;
         } else {
             echo $pingback->getErrorSummary();
         }
+    }
+
+    public function updateRecurringOrder($orderInfo, $ref, $transactionStatus, $recurringStatus)
+    {
+        try {
+            $orderRecurring = $this->db->query("SELECT * FROM `" . DB_PREFIX . "order_recurring` WHERE `order_id` = {$orderInfo['order_id']} ORDER BY order_recurring_id DESC LIMIT 1")->row;
+            if (!empty($orderRecurring)) {
+                $this->load->model('checkout/recurring');
+                $orderRecurringId = $orderRecurring['order_recurring_id'];
+                if (!empty($orderRecurring['profile_reference'])) {
+                    $orderRecurring['name'] = $orderRecurring['product_name'];
+                    $orderRecurring['quantity'] = $orderRecurring['product_quantity'];
+                    $orderRecurring['description'] = $orderRecurring['profile_description'];
+                    $orderRecurring['recurring_trial'] = $orderRecurring['trial'];
+                    $orderRecurring['recurring_trial_cycle'] = $orderRecurring['trial_cycle'];
+                    $orderRecurring['recurring_trial_duration'] = $orderRecurring['trial_duration'];
+                    $orderRecurring['recurring_trial_price'] = $orderRecurring['trial_price'];
+
+                    $orderRecurringId = $this->model_checkout_recurring->create($orderRecurring, $orderInfo['order_id'], '');
+                }
+                $this->model_checkout_recurring->addReference($orderRecurringId, $ref);
+
+                $amount = $orderInfo['currency_value'] > 0
+                    ? ($orderInfo['total'] * $orderInfo['currency_value'])
+                    : $orderInfo['total'];
+                $this->updateRecurringTransaction($orderRecurringId, $amount, $transactionStatus, $recurringStatus);
+            }
+        } catch (Exception $ex) {}
+    }
+
+    protected function updateRecurringTransaction($order_recurring_id, $amount, $transactionStatus, $recurringStatus) {
+        try {
+            $this->db->query("INSERT INTO `" . DB_PREFIX . "order_recurring_transaction` SET `order_recurring_id` = {$order_recurring_id}, `created` = NOW(), `amount` = {$amount}, `type` = {$transactionStatus}");
+            $this->db->query("UPDATE `" . DB_PREFIX . "order_recurring` SET `status` = {$recurringStatus} WHERE `order_recurring_id` = {$order_recurring_id} LIMIT 1");
+        } catch (Exception $ex) {}
     }
 
     /**
